2f04aad39058d0c8cdfb7138fad9e6f5d0fa3a3b
wangliang <3130187893@qq.com>
2025-09-30 07:42:13 +0800

    更新 SendTicket.vue 组件，修改有效期字段名称，添加单主体可用数量和是否发放选项，调整表单数据结构以支持新功能。

--- piao_frontend/src/views/admin/SendTicket.vue
+++ piao_frontend/src/views/admin/SendTicket.vue
@@ -15,17 +15,25 @@
         <t-input-number autoWidth v-model="formData.ticketNum" max="10" min="1" />
       </t-form-item>
 
-      <t-form-item label="有效期" name="ticketTime">
-
+      <t-form-item label="有效期" name="vaildTime">
+        <t-date-range-picker
+          v-model="formData.vaildTime"
+          allow-input
+          clearable
+          format="YYYY-MM-DD HH:mm:ss"
+          :default-time="['00:00:00', '22:22:22']"
+          @pick="onPick"
+          @change="onChange"
+        />
       </t-form-item>
 
-      <t-form-item label="回调接口地址" name="callbackUrl">
-        <t-input v-model="formData.callbackUrl"></t-input>
+      <t-form-item label="单主体可用数量" name="userLimit">
+        <t-input-number v-model="formData.userLimit" max="10000" min="1" />
       </t-form-item>
 
-      <t-form-item label="回调接口信息" name="callbackInfo">
-        <t-textarea v-model="formData.callbackInfo" placeholder="JSON 字段" clearable />
-      </t-form-item>
+      <t-form-item label="是否发放" name="status">
+      <t-switch v-model="formData.status"></t-switch>
+    </t-form-item>
 
       <t-form-item>
         <t-space size="small">
@@ -43,37 +51,39 @@ import {
   FormProps,
   CheckboxGroupProps,
   SelectProps,
-  CascaderProps,
+  CascaderProps
 } from 'tdesign-vue-next'
+
 const formData: FormProps['data'] = reactive({
-  ticketName: '',
-  ticketEsc: '',
-  callbackUrl: '',
-  callbackInfo: '',
+  ticketId: '',
+  ticketNum: 1,
+  vaildTime: '',
+  userLimit: 1,
+  status: 0,
 })
 const courseOptions: CheckboxGroupProps['options'] = [
   {
     label: '语文',
-    value: '1',
+    value: '1'
   },
   {
     label: '数学',
-    value: '2',
+    value: '2'
   },
   {
     label: '英语',
-    value: '3',
-  },
+    value: '3'
+  }
 ]
 const TICKET_OPTIONS: SelectProps['options'] = [
   {
     label: '面试鸭100积分充值券',
-    value: 1,
+    value: 1
   },
   {
     label: '编程导航Vip会员充值券',
-    value: 2,
-  },
+    value: 2
+  }
 ]
 const ADDRESS_OPTIONS: CascaderProps['options'] = [
   {
@@ -82,9 +92,9 @@ const ADDRESS_OPTIONS: CascaderProps['options'] = [
     children: [
       {
         label: '南京市',
-        value: 300,
-      },
-    ],
+        value: 300
+      }
+    ]
   },
   {
     label: '上海',
@@ -92,9 +102,9 @@ const ADDRESS_OPTIONS: CascaderProps['options'] = [
     children: [
       {
         label: '徐汇区',
-        value: 400,
-      },
-    ],
+        value: 400
+      }
+    ]
   },
   {
     label: '四川',
@@ -102,16 +112,21 @@ const ADDRESS_OPTIONS: CascaderProps['options'] = [
     children: [
       {
         label: '成都市',
-        value: 500,
-      },
-    ],
-  },
+        value: 500
+      }
+    ]
+  }
 ]
 const onReset: FormProps['onReset'] = () => {
   MessagePlugin.success('重置成功')
 }
 const onSubmit: FormProps['onSubmit'] = ({ validateResult, firstError }) => {
   if (validateResult === true) {
+    const postData = {...formData}
+    if (formData.vaildTime) {
+      postData.startTime = formData.vaildTime[0]
+      postData.endTime = formData.vaildTime[1];
+    }
     MessagePlugin.success('提交成功')
   } else {
     console.log('Errors: ', validateResult)
